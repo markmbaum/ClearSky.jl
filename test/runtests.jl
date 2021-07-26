661536c5e4e5a9738acf4f94a26eb0442cdc7dd6
Mark Baum <markmbaum@protonmail.com>
2021-07-26 16:38:26 -0400

    test downloading in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,11 @@ using ClearSky: faddeyeva, MOLPARAM
 
 @testset "Faddeyeva" begin include("test_faddeyeva.jl") end
 @testset "Molparam" begin include("test_molparam.jl") end
+
+using Downloads
+
+fn = Downloads.download("https://www.dropbox.com/s/qdtyrdlkzcuekoc/CH4.par?dl=0")
+ν = LinRange(1, 2500, 2500)
+Ω = AtmosphericDomain((100,350), 12, (0.9,2e5), 24)
+co2 = WellMixedGas(fn, 270e-6, ν, Ω)
+println(co2.name)
